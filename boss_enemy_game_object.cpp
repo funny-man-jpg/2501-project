41291fb7ccb6ad3fa66794006e2a4036ab55b94b
Sabin Caragea <Sabin Caragea>
2025-04-08 22:05:03 -0400

    added more stuff to the boss class, still unfinished

--- boss_enemy_game_object.cpp
+++ boss_enemy_game_object.cpp
@@ -1,9 +1,16 @@
 #include "boss_enemy_game_object.h"
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace game {
 	// Constructor
 	BossEnemyObject::BossEnemyObject(const glm::vec3& position, Geometry* geom, Shader* shader, GLuint texture, PlayerGameObject* target)
 		: EnemyGameObject(position, geom, shader, texture, target) {
+		base_part_ = new BossPart(position, geom, shader, texture, target);
+		gun_arm_ = new BossPart(position, geom, shader, texture, target);
+		barrel_ = new BossPart(position, geom, shader, texture, target);
+
+		base_part_->AddChild(gun_arm_);
+		gun_arm_->AddChild(barrel_);
 	}
 
 	// handle shooting
@@ -24,4 +31,70 @@ namespace game {
 	{
 		GameObject::Update(delta_time, textures);
 	}
+
+
+BossPart::BossPart(const glm::vec3& position, Geometry* geom, Shader* shader, GLuint texture, PlayerGameObject* target, BossPart* parent)
+	: EnemyGameObject(position, geom, shader, texture, target)
+{
+}
+void BossPart::Update(double delta_time, GLuint* textures) {
+	// Track the player
+	glm::vec3 direction_to_player = glm::normalize(target_->GetPosition() - position_);
+	angle_ = atan2(direction_to_player.y, direction_to_player.x);
+
+	// Update local transform
+	glm::mat4 scaling = glm::scale(glm::mat4(1.0f), glm::vec3(scale_, 1.0f));
+	glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), angle_, glm::vec3(0, 0, 1));
+	glm::mat4 translation = glm::translate(glm::mat4(1.0f), position_);
+	local_transform_ = translation * rotation * scaling;
+
+	// If we have a parent, apply parent's world transform
+	if (parent_) {
+		world_transform_ = parent_->world_transform_ * local_transform_;
+	}
+	else {
+		world_transform_ = local_transform_;
+	}
+
+	// Propagate to children
+	for (auto* child : children_) {
+		child->Update(delta_time, textures);
+	}
+}
+
+Projectile* BossPart::Shoot(GLuint* textures)
+{
+	if (firing_timer_->Finished() && glm::length(target_->GetPosition() - position_) <= SHOOTING_RANGE && !exploding_
+		&& (state_ == pursuit || state_ == intercepting) && !target_->GetExploding()) {
+		firing_timer_->Start(ENEMY_FIRING_COOLDOWN);
+
+		// fire a basic projectile
+		return new Projectile(position_ + radius_ * GetBearing(), geometry_, shader_, textures[tex_attacker_bullet], GetBearing(), player);
+	}
+
+	return nullptr;
+}
+void BossPart::Render(glm::mat4 view_matrix, double current_time) {
+	shader_->Enable();
+
+	shader_->SetUniform1i("ghost", ghost_);
+	shader_->SetUniform1f("tex_size", tex_size_);
+	shader_->SetUniformMat4("view_matrix", view_matrix);
+
+	shader_->SetUniformMat4("transformation_matrix", world_transform_);
+
+	geometry_->SetGeometry(shader_->GetShaderProgram());
+	glBindTexture(GL_TEXTURE_2D, texture_);
+	glDrawElements(GL_TRIANGLES, geometry_->GetSize(), GL_UNSIGNED_INT, 0);
+
+	// Recursively draw children
+	for (auto* child : children_) {
+		child->Render(view_matrix, current_time);
+	}
+}
+
+void BossPart::AddChild(BossPart* child) {
+	children_.push_back(child);
+	child->parent_ = this;
+}
 }
--- boss_enemy_game_object.h
+++ boss_enemy_game_object.h
@@ -2,6 +2,7 @@
 #define BOSS_ENEMY_GAME_OBJECT_H_
 
 #include "enemy_game_object.h"
+#include<vector>
 
 #define SHOOTING_RANGE 5.0
 
@@ -14,7 +15,26 @@ namespace game {
 			// handle shooting
 			virtual Projectile* Shoot(GLuint* textures);
 			void Update(double delta_time, GLuint* textures) override;
+		private:
+			BossPart* base_part_;
+			BossPart* gun_arm_;
+			BossPart* barrel_;
 	};
+
+
+	class BossPart : public EnemyGameObject {
+	public:
+		BossPart(const glm::vec3& position, Geometry* geom, Shader* shader, GLuint texture, PlayerGameObject* target, BossPart* parent = nullptr);
+
+		glm::mat4 local_transform_;
+		glm::mat4 world_transform_;
+		BossPart* parent_;
+		std::vector<BossPart*> children_;
+		void Update(double delta_time, GLuint* textures) override;
+		Projectile* Shoot(GLuint* textures);
+		void AddChild(BossPart* child);
+	};
+
 }
 
 #endif
